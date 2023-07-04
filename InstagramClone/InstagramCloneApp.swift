e493e32e7ce8982e671f37ae7e85ae9d937d570f
bojanangelis <bojanangeleski98@gmail.com>
2023-07-04 10:16:44 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Bojan Angjeleski on 4.7.23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
